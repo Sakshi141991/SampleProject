aeba81ffebe1d7490b19dd726b0d8bc4d437cc9a
Sakshi141991 <sakshi.sachdev91@gmail.com>
2017-06-23 14:49:38 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ restService.use(bodyParser.urlencoded({
 restService.use(bodyParser.json());
 
 restService.post('/echo', function(req, res) {
-    var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
+var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.date ? "Date is::"+req.body.result.parameters.date : "Seems like some problem. Speak again."
+
+ var speech=req.body.result.parameters.date 
     return res.json({
         speech: speech,
         displayText: speech,
